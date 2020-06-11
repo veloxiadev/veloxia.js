425c0bdda13169a709473eceb4846a28f94db70d
Viktor Svensson <viktor@veloxia.se>
2020-06-11 20:21:44 +0200

    refactoring

---
+++ lib/string/boolean.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Converts a boolean value into `yes` or `no`.
+ * @param input Input boolean or truthy/falsy value.
+ * @param onTrue String to display if input is truthy.
+ * @param onFalse String to display if input is falsy.
+ */
+export declare function booleanToString(input: boolean | number, onTrue?: string, onFalse?: string): string;
---
+++ lib/string/number.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Converts a number into a string with thousands separator, decimal point etc.
+ * @param num Input number
+ * @param decimals
+ * @param decimalPoint
+ * @param thousandsSeparator
+ */
+export declare function numberFormat(num: number, decimals?: number, decimalPoint?: string, thousandsSeparator?: string): string;
